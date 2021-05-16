590a9d60824c81c61228aad6afe543c86ea86997
Vlad Zaharia <vlad.zaharia95@gmail.com>
2021-05-16 13:43:07 +0300

    Added routes

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load env vars
 
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
+
+//Mount router
+app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT;
+
+
+
 
 app.listen(PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
